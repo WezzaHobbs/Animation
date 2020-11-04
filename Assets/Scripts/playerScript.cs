e7a9a818d604a193e6e62ad0b4f5bef29e69aff3
WezzaHobbs <WezzaHobbs@users.noreply.github.com>
2020-11-04 16:01:10 +0000

    04/11/20
    adding animations

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class playerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Animator anim;
+    Rigidbody2D rb;
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        anim.SetBool("walk-down", false);
+        anim.SetBool("walk-up", false);
+        anim.SetBool("walk-right", false);
+        anim.SetBool("walk-left", false);
     }
 
     // Update is called once per frame
@@ -20,23 +26,42 @@ public class NewBehaviourScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            anim.setBool("walk-left", true);
+            anim.SetBool("walk-left", true);
             xVelocity = -speed;
         }
+        else
+        {
+            anim.SetBool("walk-left", false);
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            anim.setBool("walk-left", true);
+            anim.SetBool("walk-right", true);
             xVelocity = speed;
         }
+        else
+        {
+            anim.SetBool("walk-right", false);
+        }
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            anim.setBool("walk-left", true);
+            anim.SetBool("walk-up", true);
             yVelocity = speed;
         }
+        else
+        {
+            anim.SetBool("walk-up", false);
+        }
+
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            anim.setBool("walk-left", true);
+            anim.SetBool("walk-down", true);
             yVelocity = -speed;
+        }
+        else
+        {
+            anim.SetBool("walk-down", false);
         }
     }
 }
